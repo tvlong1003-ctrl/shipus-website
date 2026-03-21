8f159df8527f6c83daf90b91d59347be7d0bc537
tvlong1003-ctrl <tvlong1003@gmail.com>
2026-03-21 10:41:38 +0700

    Implement routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import ShippingCalculator from './components/ShippingCalculator';
 import ProcessSection from './components/ProcessSection';
 import PricingTable from './components/PricingTable';
+import Auth from './components/Auth';
 import Footer from './components/Footer';
 import SocialContact from './components/SocialContact';
+import TrackingDashboard from './components/TrackingDashboard';
+
+const HomePage = () => (
+      <div className="min-h-screen bg-white">
+        <Header />
+        <main>
+          <HeroSection />
+          <ShippingCalculator />
+          <ProcessSection />
+          <PricingTable />
+          <section id="auth-section">
+            <Auth />
+    </section>
+    </main>
+    <Footer />
+        <SocialContact />
+    </div>
+);
 
 function App() {
-    return (
-          <div className="min-h-screen bg-surface font-sans text-gray-800 flex flex-col overflow-x-hidden">
-            <Header />
-            <main className="flex-grow">
-              <HeroSection />
-              <ShippingCalculator />
-              <ProcessSection />
-              <PricingTable />
-      </main>
-        <Footer />
-
-            <SocialContact />
-      </div>
-    );
+      return (
+              <BrowserRouter>
+                <div className="overflow-x-hidden">
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+          <Route path="/tracking" element={<TrackingDashboard />} />
+    </Routes>
+    </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
